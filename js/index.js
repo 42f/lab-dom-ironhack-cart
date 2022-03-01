bbf4cc7ec933ef0d47a4982b5c557a8390daed8a
bvalette <bvalette@student.42.fr>
2022-03-01 16:50:44 +0100

    done [up to iteration 4]

--- js/index.js
+++ js/index.js
@@ -1,42 +1,65 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  const price = parseFloat(product.querySelector('.price span').textContent);
+  const quantity = parseFloat(product.querySelector('.quantity input').value);
+  const total = price * quantity;
+  product.querySelector('.subtotal span').innerHTML = total;
+  return total;
+}
 
-  //... your code goes here
+function fetchProducts() {
+  return document.querySelectorAll('.product');
+}
+
+function getProductName(product) {
+  return product.querySelector('.name span').innerHTML;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+
+  const products = fetchProducts();
+  let totalSum = 0;
+  products.forEach(product => {
+    totalSum += updateSubtotal(product);
+  })
 
   // ITERATION 3
-  //... your code goes here
+  document.querySelector('#total-value span').innerHTML = totalSum;
 }
 
 // ITERATION 4
 
 function removeProduct(event) {
+  console.log('Hello');
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+  if (target) {
+    const nodeToRemove = target.parentNode.parentNode;
+    nodeToRemove.parentNode.removeChild(nodeToRemove);
+    calculateAll();
+  }
 }
 
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+
 }
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
+  const products = fetchProducts();
+  products.forEach(product => {
+    product.querySelector('button').addEventListener('click', removeProduct);
+  })
 
   //... your code goes here
 });
